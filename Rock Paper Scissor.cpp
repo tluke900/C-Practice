6359d8a963e12a856f1cd63199c5947e900a8c9f
tluke900 <123126251+tluke900@users.noreply.github.com>
2026-09-15 16:20:48 -0500

    First C++ Practice
    Rock Paper Scissors game play, includes running score.

---
+++ Rock Paper Scissor.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+int matchMove(string playerMove) {
+    if (playerMove == "rock"||playerMove == "Rock") {
+        return 0;
+    } else if (playerMove == "paper"||playerMove == "Paper") {
+        return 1;
+    } else if (playerMove == "scissors"||playerMove == "Scissors") {
+        return 2;
+    } else {
+        cout << "Invalid move. Please choose rock, paper, or scissors." << endl;
+        return -1; // Invalid move
+    }
+}
+
+int main() {
+    string playerMove="";
+    int playerMoveInt;
+    int computerMoveInt = rand() % 3;
+
+    int playerWins=0;
+    int computerWins=0;
+
+    cin >> playerMove;
+    playerMoveInt = matchMove(playerMove);
+
+    if (playerMoveInt == -1) {
+        cout << "Exiting the game due to invalid input." << endl;
+        return 1; // Exit if invalid move
+    }
+    else if (playerMoveInt== computerMoveInt){
+        cout << "It's a tie!" << endl;
+    }
+    else if (playerMoveInt==2 && computerMoveInt==0){
+        computerWins++;
+        cout << "Computer wins!\n Score: playerWins=" << playerWins << ", ComputerWins=" << computerWins << endl;
+        
+    }
+    
+    else if (playerMoveInt>computerMoveInt){
+        playerWins++;
+        cout << "Player wins!\n Score: playerWins=" << playerWins << ", ComputerWins=" << computerWins << endl;
+
+    }
+    else{
+        computerWins++;
+        cout << "Computer wins!\n Score: playerWins=" << playerWins << ", ComputerWins=" << computerWins << endl;
+    }
+
+    return 0;
+}
